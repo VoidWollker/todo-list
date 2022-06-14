4175168722f42790175be88f16e6bdaa24de8ccf
Roman Nichi <Roman.Nichi.O@gmail.com>
2022-06-14 15:14:44 +0400

    task can bee done

--- src/Components/TodoItem/TodoItem.jsx
+++ src/Components/TodoItem/TodoItem.jsx
@@ -1,20 +1,32 @@
-import React from "react"
+import React, { useState } from "react"
 import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 import {  blue, green, red  } from '@mui/material/colors'
 import './TodoItem.scss'
 import { Checkbox, IconButton } from "@mui/material";
 
 export default function TodoItem({todo}){
+    const [todoToogle, setTodoToogle] = useState(false)
+    const [todoName, setTodoName] = useState(todo)
+
+    const handleChange = (event) =>{
+        setTodoToogle(event.target.checked)
+        if (!todoToogle){setTodoName(<s>{todo}</s>)}
+        else {setTodoName(todo)}
+    }
+
     return(
         <div className="todoItem">
             <div className="todoItem-todoName">
-                <Checkbox sx={{
-                    color: blue[400],
-                    '&.Mui-checked':{
-                        color: green[500]
-                    }
+                <Checkbox 
+                    checked={todoToogle}
+                    onChange={e => handleChange(e)}
+                    sx={{
+                        color: blue[400],
+                        '&.Mui-checked':{
+                            color: green[500]
+                        }
                 }}/>
-                <p>{todo}</p>
+                <p>{todoName}</p>
             </div>
             <IconButton>
                 <HighlightOffIcon sx={{color: red[600],}}/>
