ae905a10d0ab804d4ed3ff1688106336d3aeb75d
Roman Nichi <Roman.Nichi.O@gmail.com>
2022-06-15 14:20:19 +0400

    refactor TodoItem to use Context

--- src/Components/TodoItem/TodoItem.jsx
+++ src/Components/TodoItem/TodoItem.jsx
@@ -1,34 +1,49 @@
-import React, { useState } from "react"
-import HighlightOffIcon from '@mui/icons-material/HighlightOff'
-import {  blue, green, red  } from '@mui/material/colors'
+import React, { useContext } from "react"
+
 import './TodoItem.scss'
+import {Context} from '../../context'
 import { Checkbox, IconButton } from "@mui/material";
+import {  blue, green, red  } from '@mui/material/colors'
+import HighlightOffIcon from '@mui/icons-material/HighlightOff'
 
-export default function TodoItem({todo}){
-    const [todoToogle, setTodoToogle] = useState(false)
-    const [todoName, setTodoName] = useState(todo)
+export default function TodoItem({title, id, completed}){
+    const dispatch = useContext(Context).dispatch
 
-    const handleChange = (event) =>{
-        setTodoToogle(event.target.checked)
-        if (!todoToogle){setTodoName(<s>{todo}</s>)}
-        else {setTodoName(todo)}
+    const cls = ['todoItem']
+
+    if (completed) {
+        cls.push('completed')
+    }
+
+    const toogleTodo = event =>{
+        dispatch({
+            type: 'toogle',
+            payload: id
+        })
+    }
+
+    const removeTodo = event =>{
+        dispatch({
+            type: 'remove',
+            payload: id
+        })
     }
 
     return(
-        <div className="todoItem">
+        <div className={cls.join(' ')}>
             <div className="todoItem-todoName">
                 <Checkbox 
-                    checked={todoToogle}
-                    onChange={e => handleChange(e)}
+                    checked={completed}
+                    onChange={toogleTodo}
                     sx={{
                         color: blue[400],
                         '&.Mui-checked':{
                             color: green[500]
                         }
                 }}/>
-                <p>{todoName}</p>
+                <p>{title}</p>
             </div>
-            <IconButton>
+            <IconButton onClick={removeTodo}>
                 <HighlightOffIcon sx={{color: red[600],}}/>
             </IconButton>
         </div>
