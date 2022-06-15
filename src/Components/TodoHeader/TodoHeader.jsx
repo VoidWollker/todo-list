eadf40a2b536f7f51730e512955505d964cc7617
Roman Nichi <Roman.Nichi.O@gmail.com>
2022-06-15 13:26:15 +0400

    change Header to Context

--- src/Components/TodoHeader/TodoHeader.jsx
+++ src/Components/TodoHeader/TodoHeader.jsx
@@ -1,12 +1,32 @@
 import { TextField } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import './TodoHeader.scss'
+import {Context} from '../../context'
 
 export default function TodoHeader({}){
+    const dispatch = useContext(Context)
+    const [todoTitle, setTodoTitle] = useState('');
+
+    const addTodo = event => {
+        if (event.key === 'Enter') {
+          dispatch({
+            type: 'add',
+            payload: todoTitle
+          })
+          setTodoTitle('')
+        }
+      }
+
     return(
         <div className="todoHeader">
             <h1>TodoList</h1>
-            <TextField variant="standard" placeholder="Enter new task"/>
+            <TextField 
+                variant="standard" 
+                placeholder="Enter new task"
+                value={todoTitle}
+                onChange={e => setTodoTitle(e.target.value)}
+                onKeyDown={addTodo}
+            />
         </div>
     )
 }
